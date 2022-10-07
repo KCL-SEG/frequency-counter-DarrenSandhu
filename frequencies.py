15f021f832981545cc4ffa50abee00b2c34aafce
DarrenSandhu <115267021+DarrenSandhu@users.noreply.github.com>
2022-10-07 23:25:22 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,6 +3,9 @@
 
 def frequencies(items):
     frequencies = {}
+    newlist = []
     for item in items:
+        newlist.append(str(item))
+    for item in newlist:
         frequencies[item] = frequencies.get(item,0) + 1
     print(frequencies)
